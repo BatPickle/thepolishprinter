c66c129f5a18d6c8b562aeac234ea865e999e3d1
BatPickle <56698568+BatPickle@users.noreply.github.com>
2025-02-10 23:29:26 -0500

    Add files via upload
    - make page more responsive and better for phones
    - used vh mainly in css to do this
    - started the links to the other pages

---
+++ selectionPage.js
@@ -0,0 +1,15 @@
+const box1Btn = document.getElementById("box1");
+const box2Btn = document.getElementById("box2");
+const box3Btn = document.getElementById("box3");
+
+box1Btn.addEventListener("click", function(){
+    window.location.href = "selectionPage.html";
+});
+
+box2Btn.addEventListener("click", function(){
+    window.location.href = "selectionPage.html";
+});
+
+box3Btn.addEventListener("click", function(){
+    window.location.href = "selectionPage.html";
+});
