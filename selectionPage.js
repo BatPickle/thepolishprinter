a1963d89f03432185ba22305821d49c93c57239e
BatPickle <56698568+BatPickle@users.noreply.github.com>
2025-02-15 18:51:16 -0500

    Add files via upload
    added all of the sections as buttons
    linked pages to buttons
    currently working on gallery

--- selectionPage.js
+++ selectionPage.js
@@ -3,13 +3,13 @@ const box2Btn = document.getElementById("box2");
 const box3Btn = document.getElementById("box3");
 
 box1Btn.addEventListener("click", function(){
-    window.location.href = "selectionPage.html";
+    window.location.href = "upcoming/upcoming.html";
 });
 
 box2Btn.addEventListener("click", function(){
-    window.location.href = "selectionPage.html";
+    window.location.href = "gallery/gallery.html";
 });
 
 box3Btn.addEventListener("click", function(){
-    window.location.href = "selectionPage.html";
+    window.location.href = "about/about.html";
 });
